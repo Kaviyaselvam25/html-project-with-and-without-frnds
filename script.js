6bdb3f347b67e52654d312fce16c581086577f8d
Kaviyaselvam25 <kaviyaselvam2504@gmail.com>
2024-07-09 09:28:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('without').addEventListener('click',function(){document.getElementById("image").src="https://th.bing.com/th?id=OIP.FkpErQJMsXVsr50yKugqdgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"});
+document.getElementById('with').addEventListener('click',function(){document.getElementById("image").src="https://th.bing.com/th/id/OIP.YVU2apBIoMh2B9VTYURO1gHaHa?w=198&h=198&c=7&r=0&o=5&pid=1.7"});
